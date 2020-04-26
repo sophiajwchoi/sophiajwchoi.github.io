f4e55ce17b4f99a2b150b50f838ec5e68203c796
sophiajwchoi <sophiac0120@gmail.com>
2020-04-25 23:50:37 -0400

    Edit styling

---
+++ js/animation.js
@@ -0,0 +1,18 @@
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+
+  $('body').css('display', 'none').fadeIn(500);
+
